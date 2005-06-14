2bb68808927f501a96aca162dd77a1e8a65a8674
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-06-14 02:30:48 +0000

    Add more Microsoft IP address ranges.
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@2233 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior-msnbot.php
+++ bad-behavior-msnbot.php
@@ -9,6 +9,8 @@ if (!defined('WP_BB_CWD'))
 
 if (matchCIDR($wp_bb_remote_addr, "207.46.0.0/16") === FALSE &&
     matchCIDR($wp_bb_remote_addr, "65.52.0.0/14") === FALSE &&
+    matchCIDR($wp_bb_remote_addr, "207.68.128.0/18") === FALSE &&
+    matchCIDR($wp_bb_remote_addr, "207.68.192.0/20") === FALSE &&
     matchCIDR($wp_bb_remote_addr, "64.4.0.0/18") === FALSE) {
 	wp_bb_spammer();
 }
