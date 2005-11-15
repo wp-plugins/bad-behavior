815f4b17131989d8fa47e3f07440579c4ce0469d
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-11-15 21:23:05 +0000

    Fix string tests that always evaluated FALSE
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@3958 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior-protocol.php
+++ bad-behavior-protocol.php
@@ -7,14 +7,14 @@ if (!defined('WP_BB_CWD'))
 
 // Is it claiming to be HTTP/1.1?  Then it shouldn't do HTTP/1.0 things
 if (!strcmp($wp_bb_server_protocol, "HTTP/1.1")) {
-	if (array_key_exists('Pragna', $wp_bb_http_headers_mixed) && !strstr($wp_bb_http_headers_mixed['Pragma'], 'no-cache') && !array_key_exists('Cache-Control', $wp_bb_http_headers_mixed)) {
+	if (array_key_exists('Pragna', $wp_bb_http_headers_mixed) && strpos($wp_bb_http_headers_mixed['Pragma'], 'no-cache') !== FALSE && !array_key_exists('Cache-Control', $wp_bb_http_headers_mixed)) {
 		wp_bb_spammer("Header 'Pragma' without 'Cache-Control' prohibited for HTTP/1.1 requests");
 	}
 }
 
 // Is it claiming to be HTTP/1.0?  Then it shouldn't do HTTP/1.1 things
 if (!strcmp($wp_bb_server_protocol, "HTTP/1.0")) {
-	if (array_key_exists('Expect', $wp_bb_http_headers_mixed) && !strstr($wp_bb_http_headers_mixed['Expect'], '100-continue')) {
+	if (array_key_exists('Expect', $wp_bb_http_headers_mixed) && strpos($wp_bb_http_headers_mixed['Expect'], '100-continue') !== FALSE) {
 		wp_bb_spammer("Header 'Expect' prohibited in HTTP/1.0 requests");
 		// TODO: return 417 for Expect: on HTTP/1.1?
 	}
