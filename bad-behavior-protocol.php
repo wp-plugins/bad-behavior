09a0383d484af136b5f157f00b97b800fde5cf01
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2005-11-16 00:56:45 +0000

    Misspelled Pragma, fixed.
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@3962 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior-protocol.php
+++ bad-behavior-protocol.php
@@ -7,7 +7,7 @@ if (!defined('WP_BB_CWD'))
 
 // Is it claiming to be HTTP/1.1?  Then it shouldn't do HTTP/1.0 things
 if (!strcmp($wp_bb_server_protocol, "HTTP/1.1")) {
-	if (array_key_exists('Pragna', $wp_bb_http_headers_mixed) && strpos($wp_bb_http_headers_mixed['Pragma'], 'no-cache') !== FALSE && !array_key_exists('Cache-Control', $wp_bb_http_headers_mixed)) {
+	if (array_key_exists('Pragma', $wp_bb_http_headers_mixed) && strpos($wp_bb_http_headers_mixed['Pragma'], 'no-cache') !== FALSE && !array_key_exists('Cache-Control', $wp_bb_http_headers_mixed)) {
 		wp_bb_spammer("Header 'Pragma' without 'Cache-Control' prohibited for HTTP/1.1 requests");
 	}
 }
