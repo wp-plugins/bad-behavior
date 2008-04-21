cee80572e3d0f36bc10c682a35022b5d8cfd9910
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-21 17:46:54 +0000

    Fix typo on admin page
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@42030 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior/admin.inc.php
+++ bad-behavior/admin.inc.php
@@ -73,8 +73,11 @@ function bb2_options()
 	<fieldset class="options">
 	<legend><?php _e('Logging'); ?></legend>
 	<p><label><input type="radio" name="logging" value="verbose" <?php if ($settings['verbose']) { ?>checked="checked" <?php } ?>/> <?php _e('Verbose HTTP request logging'); ?></label></p>
-	<p><label><input type="radio" name="logging" value="normal" <?php if ($settings['logging'] && !$settings['verbose']) { ?>checked="checked" <?php } ?>/> <?php _e('Normal HTTP request logging'); ?></label></p>
-	<p><label><input type="radio" name="logging" value="false" <?php if (!$settings['logging']) { ?>checked="checked" <?php } ?>/> <?php _e('Normal HTTP request logging'); ?></label></p>
+	<p><label><input type="radio" name="logging" value="normal" <?php if ($settings['logging'] && !$settings['verbose']) { ?>checked="checked" <?php } ?>/> <?php _e('Normal HTTP request logging (recommended)'); ?></label></p>
+	<p><label><input type="radio" name="logging" value="false" <?php if (!$settings['logging']) { ?>checked="checked" <?php } ?>/> <?php _e('Do not log HTTP requests (not recommended)'); ?></label></p>
+	</fieldset>
+
+	<fieldset class="options">
 	<legend><?php _e('Strict Mode'); ?></legend>
 	<p><label><input type="checkbox" name="strict" value="true" <?php if ($settings['strict']) { ?>checked="checked" <?php } ?>/> <?php _e('Strict checking (blocks more spam but may block some people)'); ?></label></p>
 	</fieldset>
