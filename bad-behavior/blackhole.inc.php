2328584791f4388cdacae0c919563366818d91d3
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-07-30 18:50:19 +0000

    Finally fixed the problem with the http:BL lookup
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@57009 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior/blackhole.inc.php
+++ bad-behavior/blackhole.inc.php
@@ -39,7 +39,7 @@ function bb2_httpbl($settings, $package) {
 	if (!$settings['httpbl_key']) return false;
 
 	$find = implode('.', array_reverse(explode('.', $package['ip'])));
-	$result = gethostbynamel("${settings['httpbl_key']}.${find}.dnsbl.httpbl.org.");
+	$result = gethostbynamel($settings['httpbl_key'].".${find}.dnsbl.httpbl.org.");
 	if (!empty($result)) {
 		$ip = explode('.', $result);
 		if ($ip[0] == 127 && ($ip[3] & 7) && $ip[2] >= $settings['httpbl_threat'] && $ip[1] >= $settings['httpbl_maxage']) {
