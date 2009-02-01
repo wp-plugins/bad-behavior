d6cf5fdc516fef3c478b0c4abf103cadecae9c8e
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-02-01 07:18:45 +0000

    Don't check blacklists for IPv6 addresses. Temporary fix until blacklists have IPv6 support.
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/trunk@90936 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior-wordpress.php
+++ bad-behavior-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bad Behavior
-Version: 2.0.25
+Version: 2.0.26
 Description: Deny automated spambots access to your PHP-based Web site.
 Plugin URI: http://www.bad-behavior.ioerror.us/
 Author: Michael Hampton
