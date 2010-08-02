9b938ac9ca6e6bf4976b082e85ffdbd627f0f804
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-08-02 02:35:02 +0000

    2.1.5
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/tags/2.1.5@270467 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ bad-behavior/cloudflare.inc.php
@@ -0,0 +1,17 @@
+<?php if (!defined('BB2_CORE')) die('I said no cheating!');
+
+// Analyze requests claiming to be from CloudFlare
+
+require_once(BB2_CORE . "/roundtripdns.inc.php");
+
+function bb2_cloudflare(&$package)
+{
+	if (bb2_roundtripdns($package['ip'], "cloudflare.com")) {
+		$package['ip'] = $package['headers_mixed']['Cf-Connecting-Ip'];
+	} else {
+		return '70e45496';
+	}
+	return false;
+}
+
+?>
