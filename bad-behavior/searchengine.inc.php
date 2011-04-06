e54ba4d682eb3762b49da94d83623f12ff037acd
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-04-06 02:55:50 +0000

    Bad Behavior 2.1.12 and 2.0.43
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/tags/2.1.12@369321 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior/searchengine.inc.php
+++ bad-behavior/searchengine.inc.php
@@ -6,7 +6,7 @@ require_once(BB2_CORE . "/roundtripdns.inc.php");
 
 function bb2_google($package)
 {
-	if (match_cidr($package['ip'], array("66.249.64.0/19", "64.233.160.0/19", "72.14.192.0/18")) === FALSE) {
+	if (match_cidr($package['ip'], array("66.249.64.0/19", "64.233.160.0/19", "72.14.192.0/18", "203.208.32.0/19")) === FALSE) {
 		return "f1182195";
 	}
 #	Disabled due to http://bugs.php.net/bug.php?id=53092
