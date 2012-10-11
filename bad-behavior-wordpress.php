f1b6c99637bf769cd8209caaeaf1779f81c4e50e
error <error@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-11 16:29:46 +0000

    Bad Behavior 2.2.11 and 2.0.49
    git-svn-id: https://plugins.svn.wordpress.org/bad-behavior/branches/2.0@610955 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bad-behavior-wordpress.php
+++ bad-behavior-wordpress.php
@@ -1,7 +1,7 @@
 <?php
 /*
 Plugin Name: Bad Behavior
-Version: 2.0.48
+Version: 2.0.49
 Description: Deny automated spambots access to your PHP-based Web site.
 Plugin URI: http://www.bad-behavior.ioerror.us/
 Author: Michael Hampton
